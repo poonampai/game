c07b02396c7c24a79f5ae75b70578e87e7518b7d
Yash Diniz <yashdiniz@gmail.com>
2020-05-03 01:38:53 +0530

    fixed serializability requirements

--- flappybird/Renderer.java
+++ flappybird/Renderer.java
@@ -3,6 +3,11 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 public class Renderer extends JPanel
 {
+    /**
+     *  Added serialVersionUID to comply with Java Serializability
+     */
+    private static final long serialVersionUID = -6973093079234550944L;
+
     @Override
     protected void paintComponent(Graphics g)
     {
